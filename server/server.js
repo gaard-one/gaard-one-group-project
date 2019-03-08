7e4f9c118bde4e9a13daf043dc947a91639cc3fc
Tiana Johnson <tiana.marie013@gmail.com>
2019-03-08 13:47:26 -0600

    routs skeleton build

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const plotsRouter = require('./routes/plots.router'); // plots router link
+const productsRouter = require('./routes/products.router');  //products router link not (s) on products
+const productTypeRouter = require('./routes/productType.router'); //product type router link
+const unitSqRouter = require('./routes/unitsSq.router'); // units sq  router link
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +28,10 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.user('/api/plots', plotsRouter);
+app.user('/api/products', productsRouter);
+app.use('/apu/productType', productTypeRouter);
+app.use('/api/unitSq', unitSqRouter);
 
 // Serve static files
 app.use(express.static('build'));

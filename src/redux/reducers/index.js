52f70bd224c5a5df7661ead84403a564b8b0b39c
Tiana Johnson <tiana.marie013@gmail.com>
2019-03-08 14:24:50 -0600

    Reducer Side build out

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,10 @@ import { combineReducers } from 'redux';
 import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
+// import product from './productReducer';
+// import productType from './productTypeReducer';
+// import unitSq from './unitSqReducer';
+// uncomment when being built out or else it will throw errors
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -13,6 +17,9 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
+  // product, 
+  // productType,
+  // unitSq,
 });
 
 export default rootReducer;

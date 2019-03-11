a321dbdf113b90d084dda4f50fb95021949b4c20
Peng Xue Vang <pxvang33@gmail.com>
2019-03-11 11:40:08 -0500

    added updateProductPrinted saga

--- src/redux/sagas/productSaga.js
+++ src/redux/sagas/productSaga.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 function* productSaga() {
     yield takeLatest('FETCH_PRODUCT', fetchProduct)
     yield takeLatest('ADD_PRODUCT', addProduct)
+    yield takeLatest('UPDATE_PRODUCT_PRINTED', updateProductPrinted)
+    yield takeLatest('UPDATE_PRODUCT_CLAIMED', updateProductClaimed)
 }
 //Create product
 function* addProduct(action) {
@@ -27,9 +29,31 @@ function* fetchProduct() {
 }
 
 //update printed 
-
+function* updateProductPrinted(action) {
+    let productPrinted = action.payload.id;
+    console.log('in updateProductPrinted payload', productPrinted);
+    console.log('in updateProductPrinted action', action);
+    try {
+        yield axios.put(`/api/product/printed/${productPrinted}`, action.payload);
+        let nextAction = { type: 'FETCH_PRODUCT'};
+        yield put(nextAction);
+    } catch (error) {
+        console.log('in updateProductPrinted PUT', error);
+    }
+}
 //update claimed
-
+function* updateProductClaimed(action) {
+    let productPrinted = action.payload.id;
+    console.log('in updateProductPrinted payload', productPrinted);
+    console.log('in updateProductPrinted action', action);
+    try {
+        yield axios.put(`/api/product/printed/${productPrinted}`, action.payload);
+        let nextAction = { type: 'FETCH_PRODUCT' };
+        yield put(nextAction);
+    } catch (error) {
+        console.log('in updateProductPrinted PUT', error);
+    }
+}
 //delete product
 
 export default productSaga;

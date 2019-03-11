00b7efc9eb6118af37185a104902c70b0fa6dc8c
Cody Troop <codytroop@yahoo.com>
2019-03-11 13:53:10 -0500

    get route for plot by id works

--- src/redux/sagas/plotSaga.js
+++ src/redux/sagas/plotSaga.js
@@ -1,5 +1,50 @@
-// create plot
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
 
 //fetch plot
+function* fetchPlot(action) {
+    try {
+        const response = yield axios.get(`api/plot/${action.payload}`);
+    
+        //sends array of rows for plot
+        yield put({ type: 'SET_DISPLAY_SQUARE', payload: response.data });
+    
+      } catch (error) {
+        console.log('Plot get request failed', error);
+      }
+}
 
 //update plot
+//will 
+
+
+
+
+// worker Saga: will be fired on "FETCH_USER" actions
+function* fetchUser() {
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
+
+    // the config includes credentials which
+    // allow the server session to recognize the user
+    // If a user is logged in, this will return their information
+    // from the server session (req.user)
+    const response = yield axios.get('api/user', config);
+
+    // now that the session has given us a user object
+    // with an id and username set the client-side user object to let
+    // the client-side code know the user is logged in
+    yield put({ type: 'SET_USER', payload: response.data });
+  } catch (error) {
+    console.log('User get request failed', error);
+  }
+}
+
+function* plotSaga() {
+  yield takeLatest('FETCH_PLOT', fetchPlot);
+}
+
+export default plotSaga;

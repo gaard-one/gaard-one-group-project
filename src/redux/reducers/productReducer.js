a83d932067a6ff44c20a77608e0a61f755d53065
Peng Xue Vang <pxvang33@gmail.com>
2019-03-11 13:59:50 -0500

    added product reducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,1 +1,8 @@
 //SET_PRODUCT
+const product = (state = [], action) => {
+    if (action.type === 'SET_PRODUCT'){
+        return action.payload
+    }
+    return state;
+}
+export default product;

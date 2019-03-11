8b8680e9a88acf3a860d7b0e88ca61807f08a678
Peng Xue Vang <pxvang33@gmail.com>
2019-03-11 12:02:18 -0500

    created deleteProduct saga

--- src/redux/sagas/productSaga.js
+++ src/redux/sagas/productSaga.js
@@ -6,9 +6,11 @@ function* productSaga() {
     yield takeLatest('ADD_PRODUCT', addProduct)
     yield takeLatest('UPDATE_PRODUCT_PRINTED', updateProductPrinted)
     yield takeLatest('UPDATE_PRODUCT_CLAIMED', updateProductClaimed)
+    yield takeLatest('DELETE_PRODUCT', deleteProduct)
 }
 //Create product
 function* addProduct(action) {
+    console.log('in addProduct saga', action.payload);
     try {
         yield axios.post('/api/product', action.payload);
         const newAction = { type: 'FETCH_PRODUCT' };
@@ -27,12 +29,11 @@ function* fetchProduct() {
         console.log('error in fetchProduct GET saga', error);
     }
 }
-
 //update printed 
 function* updateProductPrinted(action) {
     let productPrinted = action.payload.id;
     console.log('in updateProductPrinted payload', productPrinted);
-    console.log('in updateProductPrinted action', action);
+    console.log('in updateProductPrinted action', action.payload);
     try {
         yield axios.put(`/api/product/printed/${productPrinted}`, action.payload);
         let nextAction = { type: 'FETCH_PRODUCT'};
@@ -43,17 +44,28 @@ function* updateProductPrinted(action) {
 }
 //update claimed
 function* updateProductClaimed(action) {
-    let productPrinted = action.payload.id;
-    console.log('in updateProductPrinted payload', productPrinted);
-    console.log('in updateProductPrinted action', action);
+    let productClaimed = action.payload.id;
+    console.log('in updatedProductClaimed payload', productClaimed);
+    console.log('in updatedProductClaimed action', action.payload);
     try {
-        yield axios.put(`/api/product/printed/${productPrinted}`, action.payload);
+        yield axios.put(`/api/product/claimed/${productClaimed}`, action.payload);
         let nextAction = { type: 'FETCH_PRODUCT' };
         yield put(nextAction);
     } catch (error) {
-        console.log('in updateProductPrinted PUT', error);
+        console.log('in updateProductClaimed PUT', error);
     }
 }
 //delete product
+function* deleteProduct(action) {
+    const product = action.payload;
+    console.log('in deleteProduct saga', product);
+    try {
+        yield axios.delete(`/api/product/${product}`);
+        let nextAction = { type: 'FETCH_PRODUCT'};
+        yield put(nextAction);
+    } catch (error) {
+        console.log('in deleteProduct saga error', error);
+    }
+}
 
 export default productSaga;

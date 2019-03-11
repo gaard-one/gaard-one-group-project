2fc17298569c8e1cfaffd32f121cd7897593f38c
Peng Xue Vang <pxvang33@gmail.com>
2019-03-11 10:31:00 -0500

    created product/claimed PUT route

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -11,11 +11,25 @@ router.put('/printed/:id', (req, res)=>{
         console.log('in /printed PUT route', responseFromDatabase);
         res.sendStatus(200);
     }).catch((error)=>{
-        console.log('Error in completed /printed PUT query', error);
+        console.log('Error in /printed PUT query', error);
         res.sendStatus(500);
     });
 })
 // end PUT route for product printed
+
+// PUT route for product claimed
+router.put('/claimed/:id', (req, res)=>{
+    console.log('in put route req.body', req.body);
+    const queryText = `UPDATE "product" SET "claimed" = true WHERE "id" =$1;`;
+    pool.query(queryText, [req.params.id])
+    .then((responseFromDatabase)=>{
+        console.log('in /claimed PUT route', responseFromDatabase);
+        res.sendStatus(200);
+    }).catch((error)=>{
+        console.log('Error in /claimed PUT query', error);
+    });
+})
+// end PUT route for product claimed
 /**
  * GET route template
  */

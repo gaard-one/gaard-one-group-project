075ea369d5e39cfd331bb13370c72c8fe7ad500e
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-03-13 08:11:03 -0500

    add QrCode reducer

--- src/components/QrCode/QrCodeGenerator.js
+++ src/components/QrCode/QrCodeGenerator.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import QRCode from 'qrcode-react';
 
-function mapStateToProps(state) {
-    return {
-
-    };
-}
 
 class QrCodeGenerator extends Component {
+    
+   
+    
     render() {
+        //these need the URL for the site and the ID data for the get 
+        let baseUrl="";
+        let data="";
         return (
             <div>
-                
+                 <QRCode
+                     value=""
+                     size={128}
+                     level={'M'}
+                     />,
+ 
             </div>
         );
     }
 }
 
-export default connect(mapStateToProps,)(QrCodeGenerator);
+export default connect()(QrCodeGenerator);

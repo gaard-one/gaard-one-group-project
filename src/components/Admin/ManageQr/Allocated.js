5ad82d5e8a8bc3e74fa948ed9d6215f8499f3fb5
Cody Troop <codytroop@yahoo.com>
2019-03-18 16:49:41 -0500

    total allocated squares displays on DOM

--- src/components/Admin/ManageQr/Allocated.js
+++ src/components/Admin/ManageQr/Allocated.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class Allocated extends Component {
+
+    componentDidMount = () => {
+        this.props.dispatch({ type: 'FETCH_ALLOCATED_SQUARES' });
+    }
+
     render() {
+        const totalSq = parseInt(this.props.unitSq.squareTotal);
+        const allocatedSq = parseInt(this.props.unitSq.allocatedSquares.length);
+
         return (
             <div>
                 <p>
-                    [insert amount] has been allocated   
+                    {allocatedSq} has been allocated out of {totalSq},
+                    or {((allocatedSq/totalSq)*100)}%
                 </p>
                
             </div>
         );
     }
 }
-export default Allocated;
+
+const mapStatetoProps = (state) => ({
+    unitSq: state.unitSq,
+});
+
+export default connect(mapStatetoProps)(Allocated);
--- src/redux/reducers/unitSqReducer.js
+++ src/redux/reducers/unitSqReducer.js
@@ -1,3 +1,6 @@
+import { combineReducers } from 'redux';
+
+
 //SET_DISPLAY_SQUARE
 const displaySquare = (state=[], action) => {
     switch (action.type) {
@@ -8,4 +11,28 @@ const displaySquare = (state=[], action) => {
     }
   }
 
-export default displaySquare;
+  const allocatedSquares = (state=[], action) => {
+    switch (action.type) {
+      case 'SET_ALLOCATED_SQUARES':
+        return action.payload;
+      default:
+        return state;
+    }
+  }
+
+  const squareTotal = (state={}, action) => {
+    switch (action.type) {
+      case 'SET_TOTAL_SQUARES':
+        return action.payload[0].count;
+      default:
+        return state;
+    }
+  }
+
+  const unitSq = combineReducers({
+    displaySquare,
+    allocatedSquares,
+    squareTotal,
+  });
+
+export default unitSq;

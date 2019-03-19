000411a0428331059c47973240ddf5df5903afa1
Cody Troop <codytroop@yahoo.com>
2019-03-19 09:50:53 -0500

    changed product get route to include products that have been printed

--- src/components/Admin/ManageQr/QrTableRow.js
+++ src/components/Admin/ManageQr/QrTableRow.js
@@ -14,14 +14,22 @@ class QrTableRow extends Component {
 //     this.pdfExportComponent.save();
 // }
 
+    printedLogic = () => {
+        if(this.props.qrproduct.printed){
+            return <TableCell>Printed</TableCell>
+        }else{
+            return <TableCell> Not Printed</TableCell>
+
+        }
+    }
+
     render() {
         // {JSON.stringify(this.state.product.id)}
         return (
            <TableRow>
                <TableCell>{this.props.qrproduct.product_name}</TableCell>
                <TableCell>{this.props.qrproduct.cost}</TableCell>
-               <TableCell>{this.props.qrproduct.printed}</TableCell>
-               
+               {this.printedLogic()}               
                     <TableCell><QRCode value={`localhost:3000/#/home/${this.props.qrproduct.id}`} /></TableCell>
            
            </TableRow>

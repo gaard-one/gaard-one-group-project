f07757563f8c10a802a49a7a06b27c9c49393f8a
Cody Troop <codytroop@yahoo.com>
2019-03-22 14:20:57 -0500

    Thank you message only appears if something was bought

--- src/components/UserLandingPage/UserLandingPage.js
+++ src/components/UserLandingPage/UserLandingPage.js
@@ -22,17 +22,25 @@ class UserLandingPage extends Component {
     });
   }
 
+  thankYou = () => {
+    let totalArea = this.props.squares.length;
+    if(totalArea > 0){
+      return(
+        <div>
+          <h3>Thank You!!!</h3>
+          <h4>Your purchase preserved {totalArea} square feet</h4>
+        </div>
+      )}
+  }
+
 
   render() {
 
-
-    let totalArea = this.props.squares.length;
     return (
       <div>
         <Header />
         <div className="ui-home-addons">
-        <h3>Thank You!!!</h3>
-        <h4>Your purchase preserved {totalArea} square feet</h4>
+        {this.thankYou()}
         
         <QRScanner />
         <BaseMap />
